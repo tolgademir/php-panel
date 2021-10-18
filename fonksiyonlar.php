c228cf13f0635532aa4384869eca869832fbdfc3
Tolga Demir <40199261+tolgademir@users.noreply.github.com>
2021-10-18 18:50:24 +0300

    1

--- fonksiyonlar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function oturumkontrol(){
-	if (!isset($_SESSION['kul_mail']) OR !isset($_SESSION['kul_isim'])  OR !isset($_SESSION['kul_id'])) {
-		session_destroy();
-		header("location:./login.php");
-		exit;
-	}
-}
-?>
